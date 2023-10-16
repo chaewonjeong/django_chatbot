561fc04446b9873f609b0e20dcfe783b7cb1a200
chaewonjeong <chaewonjeong.dev@gmail.com>
2023-10-17 00:55:05 +0900

    sendAsk() 함수 기능 구체화 및 모델 삽입 준비

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,8 +1,24 @@
+import json
+import os
+import torch
 from django.shortcuts import render
-
-# Create your views here.
+from django.core.cache import cache
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from transformers import GPT2LMHeadModel
+from transformers import PreTrainedTokenizerFast
 
 
 def home(request):
     context = {}
     return render(request, "home.html", context)
+
+
+def chatanswer(request):
+    context = {}
+
+    chattext = request.GET["chattext"]
+
+    return JsonResponse(context, content_type="application/json")
+
+    return JsonResponse(context, content_type="application/json")
